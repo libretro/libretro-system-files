183bf260e70fa5bfce4dd8a26a314894e5afce16
Giovanni Cascione <ing.cascione@gmail.com>
2023-01-15 12:03:16 +0100

    add support for branches different from master

--- scripts/lib/common.sh
+++ scripts/lib/common.sh
@@ -9,11 +9,13 @@ OUT_DIR="$(realpath "${LIB_DIR}/../out")"
 # updating local clone if it already exists
 #    $1: repo url
 #    $2: local repo directory name
-function update_src_repo() 
+#    $3: repo branch, optional
+function update_src_repo()
 {
 	local repo_url="$1"
 	local repo_name="$2"
 	local repo_path="${SRC_REPOS_DIR}/${repo_name}"
+	[ -z "$3" ] && local repo_branch="master" || local repo_branch="$3"
 
 	# Sanity check
 	if [[ -z "$repo_url" || -z "$repo_name" ]]
@@ -26,14 +28,14 @@ function update_src_repo()
 	then
 		# If path exists, update local repository
 		cd "$repo_path"
-		if ! git fetch --depth 1 && git reset --hard origin/master && git clean -dfx
+		if ! git fetch --depth 1 && git reset --hard origin/"$repo_branch" && git clean -dfx
 		then
 			echo "update_src_repo: failed to update repository $repo_name..."
 			exit 2
 		fi
 	else
 		# Otherwise clone a new copy
-		if ! git clone -b master "$repo_url" --depth=1 "$repo_path"
+		if ! git clone -b "$repo_branch" "$repo_url" --depth=1 "$repo_path"
 		then
 			echo "update_src_repo: failed to clone repository $repo_name..."
 			exit 3
