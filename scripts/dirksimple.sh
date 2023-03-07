fd3f13df84c6582f15f13c4fbdf996f84e7c8c4b
hunterk <hunter_kaller@yahoo.com>
2023-03-06 21:41:59 -0600

    add packaging of DirkSimple game files

---
+++ scripts/dirksimple.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(dirname $(which $0))"
+source "$(realpath "${SCRIPT_DIR}/lib/common.sh")"
+
+REPO_URL="https://github.com/icculus/DirkSimple.git"
+REPO_NAME="DirkSimple"
+REPO_PATH="${SRC_REPOS_DIR}/${REPO_NAME}"
+
+SYSTEM_DIR="DirkSimple"
+ARCHIVE_FILE="${OUT_DIR}/DirkSimple.zip"
+
+if ! update_src_repo "$REPO_URL" "$REPO_NAME"
+then
+	exit 1
+fi
+
+cd "$BUILD_DIR"
+
+rm -rf "$SYSTEM_DIR"
+mkdir "${SYSTEM_DIR}"
+cp -r "${REPO_PATH}/data" "$SYSTEM_DIR"
+cp -r "${REPO_PATH}/LICENSE.txt" "${SYSTEM_DIR}
+
+rm -f "$ARCHIVE_FILE"
+7z a -mx9 "$ARCHIVE_FILE" "$SYSTEM_DIR"
+rm -rf "$SYSTEM_DIR"
