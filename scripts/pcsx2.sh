d3f06d36064188ecb7f43eb3665d64cab7911dc2
hunterk <hunter_kaller@yahoo.com>
2024-12-24 14:05:32 -0600

    re-add pcsx2 script
    to put the GameIndex file in the resources dir

---
+++ scripts/pcsx2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(dirname $(which $0))"
+source "$(realpath "${SCRIPT_DIR}/lib/common.sh")"
+
+REPO_URL="https://git.libretro.com/libretro/ps2.git"
+REPO_NAME="ps2"
+REPO_BRANCH="libretroization"
+REPO_PATH="${SRC_REPOS_DIR}/${REPO_NAME}"
+
+SYSTEM_DIR="pcsx2"
+ARCHIVE_FILE="${OUT_DIR}/LRPS2.zip"
+
+if ! update_src_repo "$REPO_URL" "$REPO_NAME" "$REPO_BRANCH"
+then
+	exit 1
+fi
+
+cd "$BUILD_DIR"
+
+rm -rf "$SYSTEM_DIR"
+mkdir -p "$SYSTEM_DIR/resources"
+mkdir -p "$SYSTEM_DIR/bios"
+cp -r "${REPO_PATH}/bin/resources/GameIndex.yaml" "$SYSTEM_DIR/resources"
+
+rm -f "$ARCHIVE_FILE"
+7z a -mx9 "$ARCHIVE_FILE" "$SYSTEM_DIR"
+rm -rf "$SYSTEM_DIR"
